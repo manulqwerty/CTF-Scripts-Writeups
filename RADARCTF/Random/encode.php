6e1f22d11b9f8918d0be24c23b38ff8749ed97c4
manulqwerty <39005141+manulqwerty@users.noreply.github.com>
2019-04-04 21:39:54 +0200

    Update encode.php

--- RADARCTF/Random/encode.php
+++ RADARCTF/Random/encode.php
@@ -1,17 +1,14 @@
 <?php
     function encode($input){ 
         $inputlen = strlen($input);
-		$randkey = 5;
+		$randkey = rand(1, 9); 
 		$i = 0;
 		while ($i < $inputlen) {
 			$inputchr[$i] = (ord($input[$i]) - $randkey);
 			$i++; 
 		}
-		print_r ($inputchr);
-		
 		$encrypted = implode('.', $inputchr) . '.' . (ord($randkey)+49);
 		return $encrypted;
     }
 	echo(encode("radar{}"));
 ?>
-
